a05cfed3b4d0559d9eec9eda27fcdab9cff69345
SFQRM <zhuojinghan@126.com>
2019-09-26 08:18:20 +0800

    Update CNN_new.py

--- CNN_new.py
+++ CNN_new.py
@@ -45,11 +45,28 @@ def max_pooling_layer_2x2(x):
     return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
 
-W_conv1 = weight_variable([5, 5, 1, 32])                    # patch:5x5 insize:1 outsize:32
-b_conv1 = bias_variable([32])
-hiden_conv1 = tf.nn.relu(convolution_layer(x_image, W_conv1) + b_conv1)
-hiden_pool1 = max_pooling_layer_2x2(hiden_conv1)
+W_conv1 = weight_variable([5, 5, 1, 32])                                # 第一层卷积层的权重, patch:5x5 insize(输入图片的厚度):1 outsize:32
+b_conv1 = bias_variable([32])                                           # 第一层卷积层的偏置
+hiden_conv1 = tf.nn.relu(convolution_layer(x_image, W_conv1) + b_conv1) # 第一层卷积层, output size 28x28x32
+hiden_pool1 = max_pooling_layer_2x2(hiden_conv1)                        # 第一层池化层, output size 14x14x32
 
+
+W_conv2 = weight_variable([5, 5, 32, 64])                               # 第二层卷积层的权重, patch:5x5 insize:32 outsize:64
+b_conv2 = bias_variable([64])                                           # 第二层卷积层的偏置
+hiden_conv2 = tf.nn.relu(convolution_layer(hiden_pool1, W_conv2) + b_conv2) # 第二层卷积层, output size 14x14x64
+hiden_pool2 = max_pooling_layer_2x2(hiden_conv2)                        # 第二层池化层, output size 7x7x64
+
+
+W_func1 = weight_variable([7*7*64, 1024])
+b_func1 = bias_variable([1024])
+hiden_pool2_flat = tf.reshape(hiden_pool2, [-1, 7*7*64])                # [n_samples,7,7,64]->>[n_samples,7x7x64]
+hiden_func1 = tf.nn.relu(tf.matmul(hiden_pool2_flat,W_func1)+b_func1)
+hiden_func1_drop = tf.nn.dropout(hiden_func1, keep_prob)
+
+
+W_func2 = weight_variable([1024, 10])
+b_func2 = bias_variable([10])
+prediction = tf.nn.softmax(tf.matmul(hiden_func1_drop, W_func2)+b_func2)
 # 定义模型-end #
 
 
@@ -64,11 +81,9 @@ sess.run(init)                                                          # 初始
 
 for i in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)                    # 随机抓取训练集中的100个批处理数据点
-    # sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 0.5})
-    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
+    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 0.5})
+    # sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
     if i % 50 == 0:                                                     # 每训练50次输出一次训练结果
         print(compute_accuracy(mnist.test.images[:1000],
                                mnist.test.labels[:1000]))
 # 训练模型-end #
-
-
