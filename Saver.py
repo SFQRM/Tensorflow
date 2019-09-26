423f0f31ad794029adaed55d7df0a8065a16ccb7
SFQRM <zhuojinghan@126.com>
2019-09-26 13:31:57 +0800

    Update Saver.py

--- Saver.py
+++ Saver.py
@@ -16,3 +16,6 @@ with tf.Session() as sess:
     save_path = saver.save(sess,"my_net/save_net.ckpt")
     print("Save to path:", save_path)
 # 保存到文件-end #
+
+
+# 注：tensorflow只能保存变量，还不能保存框架等，如需框架还需要重新构造
